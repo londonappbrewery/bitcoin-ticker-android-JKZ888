6043ef26407611e094697cd51ea891d46c53e7f1
= <jbcousinat@gmail.com>
2018-01-25 14:09:46 +0100

    working app with update of BTC price converted in fiat

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,19 +12,28 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
 
     // Member Variables:
     TextView mPriceTextView;
+    Double BTCvalue;
+
+    // API PUBLIC KEY to use api data
+    private final String PUBLIC_KEY = "NmE5NTE1ZGJiNjQ0NDc3ZGI0NjY5ODFiMzk0MWZmY2M";
+    private final String SECRET_KEY = "NjdjYzQxOTlkY2E1NDJmMWFkM2NlZGNmMWNhMzkwN2FjYTg2MTYzMzA1MWU0YTViYWI1NGJjODFkMWIwOTc3OA";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +55,52 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Set an OnItemSelected listener on the spinner
 
+spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+    @Override
+    public void onItemSelected(AdapterView<?> adapter, View view, int i, long l) {
+        Log.d("DEBUGBYJB", String.valueOf(adapter.getItemAtPosition(i)));
+        letsDoSomeNetworking(BASE_URL+String.valueOf(adapter.getItemAtPosition(i)));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d("DEBUGBYJB", "nothing was selected");
+    }
+});
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
 
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 
+                try {
+                    response.getDouble("ask");
+                    BTCvalue = response.getDouble("ask");
+                    mPriceTextView.setText(Double.toString(BTCvalue));
+
+                    Log.d("debugjjb", "JSON: "+response.getString("ask"));
+                    // called when response HTTP status is "200 OK"
+                    Log.d("jbdebug", "JSON: " + response.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("jbdebug", "Request fail! Status code: " + statusCode);
+                Log.d("jbdebug", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+                //   Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-
 }
